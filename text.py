3255ec59cb53b72cf3d668bf863e6a0e028f7058
matusoff <94809104+matusoff@users.noreply.github.com>
2022-11-30 11:23:01 -0500

    Update text.py

--- text.py
+++ text.py
@@ -4,6 +4,6 @@
 from gtts import gTTS
 from playsound import playsound
 text = "your text"
-var = gTTS(text = text, lang='en', slow=True)
+var = gTTS(text = text, lang='en')
 var.save('your path to save the file\text.mp3')
 playsound('your path to save the file\text.mp3')
